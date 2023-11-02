ae448b2ff7e4ea87963968a9a0e7b4a614fd4e8c
Hector Beita <hecbeita1988@gmail.com>
2023-11-01 23:57:33 -0600

    Updates for better easier mappping they change all the time!!! (shrug time)

--- sea_mep_csv_exporter.rb
+++ sea_mep_csv_exporter.rb
@@ -25,22 +25,30 @@ class SeaMepCsvExporter
       register = xls_file.sheets[0]
       ids = xls_file.sheets[1]
 
-      identities = ids.rows.map { |row| { id: row[1], name: row[2] } }
+      identities = ids.rows.map { |row| { id: row[3], name: row[4] } } # TODO: read from a different csv file with all names + ids
       # create a new csv file
       CSV.open("export_csv/#{registro_csv_name}.csv", "wb", col_sep: ';') do |csv|
         csv << ['id', 'Nombre', 'Trabajo cotidiano', 'Tareas', 'Prueba', 'Asistencia'].map { |value| value.to_s.strip }
 
+        name_idx = alphabet_index('C')
+        cotidiano_idx = alphabet_index('D')
+        tareas_idx = alphabet_index('E')
+        exam_1_idx = alphabet_index('H')
+        exam_2_idx = alphabet_index('K')
+        exam_3_idx = alphabet_index('P')
+        asistencia_idx = alphabet_index('L')
+
         register.rows.each_with_index do |row, index|
-          id = row[0]
-          name = row[2]
+          id = row[alphabet_index('A')]
+          name = row[name_idx]
           identity_found = identities.detect { |identity| identity[:name] == name }
           cedula = identity_found[:id] unless identity_found.nil?
-          cotidiano = row[3].nil? ? 0 : n_to_float(row[3])
-          tareas = row[4].nil? ? 0 : n_to_float(row[4])
-          asistencia = row[11].nil? ? 0 : n_to_float(row[11])
-          exam_one = row[7].nil? ? 0 : n_to_float(row[7])
-          exam_two = row[10].nil? ? 0 : n_to_float(row[10])
-          exam_three = row[17].nil? ? 0 : n_to_float(row[17])
+          cotidiano = row[cotidiano_idx].nil? ? 0 : to_float(row[cotidiano_idx])
+          tareas = row[tareas_idx].nil? ? 0 : to_float(row[tareas_idx])
+          asistencia = row[asistencia_idx].nil? ? 0 : to_float(row[asistencia_idx])
+          exam_one = row[exam_1_idx].nil? ? 0 : to_float(row[exam_1_idx]) #H
+          exam_two = row[exam_2_idx].nil? ? 0 : to_float(row[exam_2_idx]) #K
+          exam_three = row[exam_3_idx].nil? ? 0 : to_float(row[exam_3_idx]) #P
           final_grade = (exam_one + exam_two + exam_three).round(2)
 
           next if id.nil? || id.is_a?(String) || !id.is_a?(Numeric)
@@ -63,16 +71,19 @@ class SeaMepCsvExporter
 
   private
 
+  def alphabet_index(letter)
+    ('A'..'Z').to_a.index(letter)
+  end
+
   def limit_to_two_decimals(number)
     (number * 100).round / 100.0
   end
 
   # TODO: CHANGE NAMING LOOKS AWFUL
-  def n_to_float(str)
+  def to_float(str)
     value = str.to_f.nil? ? 0 : str.to_f
     value.is_a?(Float) ? value.round(2) : value.to_i
   end
-
 end
 
 
@@ -80,4 +91,4 @@ puts 'Running SeaMepCsvExporter'
 files = Dir.glob('registros/*.xlsx')
 clean = true
 
-SeaMepCsvExporter.new(files:, clean: true).run
+SeaMepCsvExporter.new(files:, clean: ).run
